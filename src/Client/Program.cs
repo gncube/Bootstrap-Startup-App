2f551ec6b958e3172c10e546e721782af69e256f
Gerald Ncube <gerald.ncube@se.com>
2023-05-15 16:23:27 +0100

    Added AddScoped for HttpClient

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -10,12 +10,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Register HttpClient service using HttpClientFactory
-builder.Services.AddHttpClient<IBlogDataService, BlogDataService>(
-       bds => bds.BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress));
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddHttpClient<IDataService<WeatherForecast, int>, WeatherService>(
-    ws => ws.BaseAddress = new Uri(builder.Configuration["WeatherAPI_Prefix"] ?? builder.HostEnvironment.BaseAddress));
+// Register HttpClient service using HttpClientFactory
+builder.Services.AddHttpClient<IBlogDataService, BlogDataService>(client =>
+    client.BaseAddress = new Uri(builder.Configuration["BlogAPI_Prefix"] ?? builder.HostEnvironment.BaseAddress));
+
+builder.Services.AddHttpClient<IDataService<WeatherForecast, int>, WeatherService>(client =>
+    client.BaseAddress = new Uri(builder.Configuration["WeatherAPI_Prefix"] ?? builder.HostEnvironment.BaseAddress));
 
 // Add JsonSerilizerOptions service
 builder.Services.AddSingleton(x => new JsonSerializerOptions
